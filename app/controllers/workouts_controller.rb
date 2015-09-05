5267ed5b055800f3ee29173422d8d4ad987e9eaa
Darwin Groskleg <darwingroskleg@gmail.com>
2015-09-05 00:49:53 -0400

    Updates workouts strong parameters, also reorders actions to convention and adds a success notice on the create action.

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,5 +1,13 @@
 class WorkoutsController < ApplicationController
 
+  def index
+    @workouts = Workout.paginate(page: params[:page], per_page: 5)
+  end
+
+  def show
+    @workout = Workout.find(params[:id])
+  end
+
   def new
     @workout = Workout.new
   end
@@ -7,24 +15,16 @@ class WorkoutsController < ApplicationController
   def create
     @workout = Workout.new(workout_params)
     if @workout.save
-      redirect_to @workout
+      redirect_to @workout, notice: 'Workout was successfully created.'
     else
       render 'new'
     end
-  end
-
-  def show
-    @workout = Workout.find(params[:id])
-  end
-
-  def index
-    @workouts = Workout.paginate(page: params[:page], per_page: 5)
   end
 
   private
 
   def workout_params
     params.require(:workout)
-      .permit(:title, :focus, :intensity, :weekday)
+      .permit(:intensity, :weekday, :workout_log)
   end
 end
